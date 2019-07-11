53ce00718e4cb4dc0a4bdaf3349b184977dce7f9
Den R <remadenn@gmail.cpm>
2019-07-12 00:26:21 +0300

    add arr in script.js and create function addLi

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
 $("#toggle-contributers").on("click", function() {
   $(".contributers__list").toggleClass("contributers__list--hide");
 });
+
+const names = ["DenRo"]; // add name to array!!!
+
+function createLi(name, parent) {
+  const contributersListName = document.createElement("li");
+  contributersListName.classList.add("contributers__list-name");
+  contributersListName.textContent = name;
+  parent.appendChild(contributersListName);
+  return contributersListName;
+}
+function init() {
+  const contributersList = document.querySelector(".contributers__list");
+  for (i = 0; i < names.length; i++) {
+    createLi(names[i], contributersList);
+  }
+}
+init();
