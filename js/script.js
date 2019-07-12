35deab9e4e4c83ac89683b84e319cd9122a1a7cf
Oleg Sh <hwind.osh@yandex.ru>
2019-07-12 17:32:26 +0300

    add slick img-roll, text edit, etc

--- js/script.js
+++ js/script.js
@@ -1,3 +1,13 @@
 $("#toggle-contributers").on("click", function() {
   $(".contributers__list").toggleClass("contributers__list--hide");
 });
+
+
+$(document).ready(function(){
+  $('.img-roll').slick({
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000
+  });
+});
